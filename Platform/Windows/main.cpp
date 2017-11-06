9b23d8d3779733f27b257bca6c199eb2678d4ac1
袁全伟 <qq573011406@126.com>
2017-11-06 19:47:25 +0800

    Sample window

--- Platform/Windows/main.cpp
+++ Platform/Windows/main.cpp
@@ -1,7 +1,78 @@
+#include <windows.h>
+#include <windowsx.h>
+#include <tchar.h>
 #include <iostream>
 
-int main(int argc,char** argv)
+
+// the WindowProc function prototype
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+bool gQuit = false;
+int main(int argc, char** argv)
 {
-    std::cout<<"Hello world!" << std::endl;
-    return 0;
+	HINSTANCE hInstance = GetModuleHandle(NULL);
+
+
+
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpszClassName = _T("XRender");
+	wc.lpfnWndProc = WindowProc;
+
+	RegisterClassEx(&wc);
+
+	HWND hWnd = CreateWindowExW(0,
+		L"XRender",      // name of the window class
+		L"XRender-Window",             // title of the window
+		WS_OVERLAPPEDWINDOW,              // window style
+		CW_USEDEFAULT,                    // x-position of the window
+		CW_USEDEFAULT,                    // y-position of the window
+		800,             // width of the window
+		600,            // height of the window
+		NULL,                             // we have no parent window, NULL
+		NULL,                             // we aren't using menus, NULL
+		hInstance,                        // application handle
+		NULL);                            // used with multiple windows, NULL
+
+	ShowWindow(hWnd, SW_SHOW);
+
+
+	// Message Loop
+	MSG msg;
+	while (!gQuit)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			// translate keystroke messages into the right format
+			TranslateMessage(&msg);
+
+			// send the message to the WindowProc function
+			DispatchMessage(&msg);
+		}
+	}
+
+	return 0;
+}
+
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	// sort through and find what code to run for the message given
+	switch (message)
+	{
+		// this message is read when the window is closed
+		case WM_DESTROY:
+		{
+			// close the application entirely
+			PostQuitMessage(0);
+			gQuit = true;
+			return 0;
+		}
+	}
+
+	// Handle any messages the switch statement didn't
+	return DefWindowProc(hWnd, message, wParam, lParam);
 }
